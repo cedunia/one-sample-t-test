d226b9aeabe5881f596b29f784d0ae5762b7e4b4
cedunia <cedric.hassenkhodja@gmail.com>
2016-07-04 14:07:50 +0200

    added assignment variables

--- scripts/t-test_statistics.R
+++ scripts/t-test_statistics.R
@@ -9,5 +9,5 @@ data_stats <- data_clean
 ## BUILD MODEL ##
 # T-TEST (base R)
 
-t.test(data_stats$systolic, mu = 127.2)
-qt(0.975, 44)
+data_stats.t_computed <- t.test(data_stats$systolic, mu = 127.2)
+data_stats.t_tabulated <- qt(0.975, 44)
